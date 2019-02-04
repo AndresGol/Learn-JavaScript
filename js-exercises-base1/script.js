48ddf83468444f3d3e63350e672bad901ad6b505
Andrés Goldestein <andresgoldestein@gmail.com>
2019-02-04 12:01:01 +0100

    premieres_exos

---
+++ js-exercises-base1/script.js
@@ -0,0 +1,17 @@
+//exercise 1
+// let first = "Hola mundo";
+// alert(first);
+
+//exercise 2
+// var nom = "Gol"
+// var prenom = "Andrés"
+// var ville = "Bruxelles"
+// alert(
+//     "Nom : "+nom+"\n"+
+//     "Prénom : " + prenom+"\n"+
+//     "Ville : "+ville
+// )
+
+//exercise 3
+prenom = prompt("Saisissez votre nom SVP");
+alert("Bonjour "+prenom);
